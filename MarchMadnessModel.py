a8d215877680c81c4e24ce92c99f43c59cb383b6
nickvirzi <navirzi@gmail.com>
2021-03-04 23:45:48 -0500

    Added support for searching a team on ESPN to gain a schedule

--- MarchMadnessModel.py
+++ MarchMadnessModel.py
@@ -1,5 +1,6 @@
 import sys
 from selenium import webdriver
+import time
 
 teamOneName = input('Enter first team name: ')
 teamTwoName = input('Enter second team name: ')
@@ -7,6 +8,31 @@ teamTwoName = input('Enter second team name: ')
 DRIVER_PATH = r'C:\Users\navir\Documents\ChromeDriver\chromedriver.exe'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 driver.get('https://kenpom.com/')
+
+def getESPNTeamInfo():
+    driver.get('https://www.espn.com/search/_/q/lsu')
+
+    window_after = driver.window_handles[0]
+    driver.switch_to.window(window_after)
+
+    elem = driver.find_element_by_xpath('//*[@id="fittPageContainer"]/div[2]/div/div/div[3]/section[1]/div/ul')
+
+    count = 1
+
+    all_li = elem.find_elements_by_tag_name("li")
+    for li in all_li:
+        text = li.text
+
+        text = text.replace('\n', ' ')
+
+        if 'NCAAM' in text:
+            break
+    
+        count += 1
+
+    elementXPATH = '//*[@id="fittPageContainer"]/div/div/div/div[3]/section[1]/div/ul/div/div[' + str(count) + ']/li'
+
+    driver.find_element_by_xpath(elementXPATH).click()
 
 class Team:
     def __init__(self,name):
@@ -26,6 +52,7 @@ class Team:
                 self.teamOffensiveRating = data[6]
                 self.teamDefensiveEfficiency = data[7]
                 self.teamDefensiveRating = data[8]
+                self.strengthOfScheduleRating = data[14]
 
         print(self.teamRank, self.teamConference)
 
@@ -34,3 +61,5 @@ teamTwo = Team(teamTwoName)
 
 teamOne.getTeamDataInfo()
 teamTwo.getTeamDataInfo()
+
+getESPNTeamInfo()
